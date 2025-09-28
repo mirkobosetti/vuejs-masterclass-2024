1cc80d1f403becb95785046a5bdc21df393e1f80
Mirko Bosetti <bosetti@cving.com>
2025-09-28 17:34:45 +0200

    feat: integrate unplugin-vue-router and set up routing structure

---
+++ src/pages/projects/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Projects page</h1>
+    <RouterLink to="/">Go to Home</RouterLink>
+  </div>
+</template>
+
+<style scoped></style>
