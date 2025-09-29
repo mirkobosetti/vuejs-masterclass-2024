19cc48a3ccc2727dbf524bcf597ceb3e3e26cfb9
Mirko Bosetti <bosetti@cving.com>
2025-09-29 10:44:57 +0200

    db seeder

--- database/seed.js
+++ database/seed.js
@@ -1,11 +1,48 @@
-// import { fakerIT as faker } from '@faker-js/faker';
-
+import { fakerIT as faker } from '@faker-js/faker'
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient(
-  process.env.VITE_SUPABASE_URL,
-  process.env.SERVICE_ROLE_KEY
-)
+const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
 
-console.log(supabase)
+const logErrorAndExit = (tableName, error) => {
+  console.error(
+    `An error occurred in table '${tableName}' with code ${error.code}: ${error.message}`
+  )
+  process.exit(1)
+}
+
+const logStep = (stepMessage) => {
+  console.log(stepMessage)
+}
+
+const seedProjects = async (numEntries) => {
+  logStep('Seeding projects...')
+  const projects = []
+
+  for (let i = 0; i < numEntries; i++) {
+    const name = faker.lorem.words(3)
+
+    projects.push({
+      name: name,
+      slug: name.toLocaleLowerCase().replace(/ /g, '-'),
+      status: faker.helpers.arrayElement(['in-progress', 'completed']),
+      collaborators: faker.helpers.arrayElements([1, 2, 3])
+    })
+  }
+
+  const { data, error } = await supabase.from('projects').insert(projects).select('id')
+
+  if (error) return logErrorAndExit('Projects', error)
+
+  logStep('Projects seeded successfully.')
+
+  return data
+}
+
+const seedDatabase = async (numEntriesPerTable) => {
+  await seedProjects(numEntriesPerTable)
+}
+
+const numEntriesPerTable = 10
+
+seedDatabase(numEntriesPerTable)
