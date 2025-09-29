1c32bc6ecb6459645aea6e898280e2819f233877
Mirko Bosetti <mirkobosetti1998@gmail.com>
2025-09-29 19:38:36 +0200

    feat: add description field to projects and update related queries and components

--- database/seed.js
+++ database/seed.js
@@ -24,6 +24,7 @@ const seedProjects = async (numEntries) => {
 
     projects.push({
       name: name,
+      description: faker.lorem.paragraph(2),
       slug: name.toLocaleLowerCase().replace(/ /g, '-'),
       status: faker.helpers.arrayElement(['in-progress', 'completed']),
       collaborators: faker.helpers.arrayElements([1, 2, 3])
