a79f2c91436cd77e586a6cc59fcf3ed2a7da9802
Mirko Bosetti <mirkobosetti1998@gmail.com>
2025-09-29 18:05:37 +0200

    feat: added Suspense loading

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -5,12 +5,13 @@ import type { ColumnDef } from '@tanstack/vue-table'
 import { RouterLink } from 'vue-router'
 
 const tasks = ref<Tables<'tasks'>[] | null>(null)
-;(async () => {
+const getTasks = async () => {
   const { data, error } = await supabase.from('tasks').select()
 
   if (error) console.log(error)
   else tasks.value = data
-})()
+}
+await getTasks()
 
 const columns: ColumnDef<Tables<'tasks'>>[] = [
   {
