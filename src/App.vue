8d85473daf01ceb9d64c29825dad78bc03e052f7
Mirko Bosetti <mirkobosetti1998@gmail.com>
2025-09-29 15:51:36 +0200

    feat: integrate @tanstack/vue-table for enhanced data table functionality and add sidebar layout

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,9 @@
 <script setup lang="ts">
-import Input from '@/components/ui/input/Input.vue'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger
-} from '@/components/ui/dropdown-menu'
+import AuthLayout from '@/components/layout/main/AuthLayout.vue'
 </script>
 
 <template>
-  <nav class="h-16 border-b bg-muted/40 flex justify-between p-6 items-center">
-    <form class="w-full max-w-96 h-fit relative">
-      <iconify-icon
-        class="absolute top-[50%] translate-y-[-50%] left-2.5 text-muted-foreground"
-        icon="lucide:search"
-      ></iconify-icon>
-      <Input class="w-full pl-8 bg-background" type="text" placeholder="Search..." />
-    </form>
-
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Avatar>
-          <AvatarImage src="https://github.com/unovue.png" alt="@unovue" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  </nav>
-  <main>
+  <AuthLayout>
     <RouterView />
-  </main>
+  </AuthLayout>
 </template>
