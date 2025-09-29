32c467aa3e2ec799eaf3b5a439e3ddc93e37c34b
Mirko Bosetti <bosetti@cving.com>
2025-09-29 12:55:51 +0200

    test tailwind 4

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
+  <nav class="h-16 border-b bg-muted/40 flex justify-between p-6 items-center">
+    <form class="w-full max-w-96">
+      <input class="w-full pl-8 bg-background" type="text" placeholder="Search..." />
+    </form>
+    <div class="w-8 h-8 rounded-full bg-white"></div>
+  </nav>
   <main>
     <RouterView />
   </main>
