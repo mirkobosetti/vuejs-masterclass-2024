abac9c9127e813b7aa134ed0ff03673420144155
Mirko Bosetti <mirkobosetti1998@gmail.com>
2025-09-29 12:50:12 +0200

    shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'unplugin-vue-router/vite'
-
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [VueRouter(), vue(), vueDevTools()],
+  plugins: [VueRouter(), vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
