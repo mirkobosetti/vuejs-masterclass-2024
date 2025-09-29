4ef4a1996ab6f43468f9cf037ab46bc822168610
Mirko Bosetti <mirkobosetti1998@gmail.com>
2025-09-29 14:29:38 +0200

    shadcn and iconify

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Input from '@/components/ui/input/Input.vue'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+</script>
 
 <template>
   <nav class="h-16 border-b bg-muted/40 flex justify-between p-6 items-center">
-    <form class="w-full max-w-96">
-      <input class="w-full pl-8 bg-background" type="text" placeholder="Search..." />
+    <form class="w-full max-w-96 h-fit relative">
+      <iconify-icon
+        class="absolute top-[50%] translate-y-[-50%] left-2.5 text-muted-foreground"
+        icon="lucide:search"
+      ></iconify-icon>
+      <Input class="w-full pl-8 bg-background" type="text" placeholder="Search..." />
     </form>
-    <div class="w-8 h-8 rounded-full bg-white"></div>
+
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src="https://github.com/unovue.png" alt="@unovue" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   </nav>
   <main>
     <RouterView />
