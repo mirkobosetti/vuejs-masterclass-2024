84251930d47ff483e3a94bc64fcb944ae4da565d
Mirko Bosetti <bosetti@cving.com>
2025-09-29 15:51:36 +0200

    feat: integrate @tanstack/vue-table for enhanced data table functionality and add sidebar layout

---
+++ src/components/layout/SidebarLinks.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface LinkProps {
+  to: string
+  icon: string
+  title: string
+}
+
+defineProps<{
+  links: LinkProps[]
+}>()
+</script>
+
+<template>
+  <RouterLink
+    v-for="link in links"
+    :key="link.title"
+    :to="link.to"
+    class="flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground"
+    active-class="bg-accent text-accent-foreground font-medium"
+  >
+    <iconify-icon :icon="link.icon"></iconify-icon>
+    <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
+  </RouterLink>
+</template>
