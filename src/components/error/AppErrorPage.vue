3352af9095fa1c9c29b14abc32b4c8556dec2371
Mirko Bosetti <mirkobosetti1998@gmail.com>
2025-09-29 21:44:27 +0200

    feat: implement error handling with AppErrorPage and useErrorStore

---
+++ src/components/error/AppErrorPage.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const router = useRouter()
+
+router.afterEach(() => {
+  useErrorStore().activeError = false
+})
+</script>
+
+<template>
+  <section
+    class="mx-auto flex justify-center items-center flex-1 p-10 text-center -mt-20 min-h-[90vh]"
+  >
+    <div>
+      <iconify-icon icon="lucide:triangle-alert" class="text-7xl text-destructive" />
+      <h1 class="font-extrabold text-7xl text-secondary">404</h1>
+      <p class="text-3xl font-extrabold text-primary">Page not found</p>
+      <div class="flex flex-col items-center justify-center gap-5 mt-6 font-light">
+        <p class="text-lg text-muted-foreground">You'll find lots to explore on the home page.</p>
+        <RouterLink to="/">
+          <Button class="max-w-36"> Back to homepage </Button>
+        </RouterLink>
+      </div>
+    </div>
+  </section>
+</template>
