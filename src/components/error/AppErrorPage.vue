b2a6771aed2a767caf1ddef119a6e4445dcaef69
Mirko Bosetti <mirkobosetti1998@gmail.com>
2025-09-30 01:32:32 +0200

    feat: add AppErrorDevSection and AppErrorProdSection components

--- src/components/error/AppErrorPage.vue
+++ src/components/error/AppErrorPage.vue
@@ -24,8 +24,12 @@ if (error.value && 'code' in error.value) {
   statusCode.value = error.value.statusCode ?? 0
 }
 
+const ErrorTemplate = import.meta.env.DEV
+  ? defineAsyncComponent(() => import('./AppErrorDevSection.vue'))
+  : defineAsyncComponent(() => import('./AppErrorProdSection.vue'))
+
 router.afterEach(() => {
-  errorStore.activeError = null
+  errorStore.clearError()
 })
 </script>
 
@@ -33,21 +37,14 @@ router.afterEach(() => {
   <section
     class="mx-auto flex justify-center items-center flex-1 p-10 text-center -mt-20 min-h-[90vh]"
   >
-    <div>
-      <iconify-icon icon="lucide:triangle-alert" class="text-7xl text-destructive" />
-      <h1 class="font-extrabold text-7xl text-secondary">{{ customCode || code }}</h1>
-      <p class="font-extrabold text-7xl text-secondary" v-if="statusCode">
-        Status Code {{ statusCode }}
-      </p>
-      <p class="text-3xl font-extrabold text-primary">{{ message }}</p>
-      <p v-if="hint">{{ hint }}</p>
-      <p v-if="details">{{ details }}</p>
-      <div class="flex flex-col items-center justify-center gap-5 mt-6 font-light">
-        <p class="text-lg text-muted-foreground">You'll find lots to explore on the home page.</p>
-        <RouterLink to="/">
-          <Button class="max-w-36"> Back to homepage </Button>
-        </RouterLink>
-      </div>
-    </div>
+    <ErrorTemplate
+      :message
+      :customCode
+      :code
+      :statusCode
+      :hint
+      :details
+      :isCustomError="errorStore.isCustomError"
+    />
   </section>
 </template>
