2fd916e27ef9f7c996a5af028e7a123655f8102b
Mirko Bosetti <bosetti@cving.com>
2025-09-30 01:07:00 +0200

    feat: supabase error handling

--- src/pages/projects/[slug].vue
+++ src/pages/projects/[slug].vue
@@ -11,9 +11,9 @@ watch(
 )
 
 const getProject = async () => {
-  const { data, error } = await projectQuery(route.params.slug as string)
+  const { data, error, status } = await projectQuery(route.params.slug as string)
 
-  if (error) console.log(error)
+  if (error) useErrorStore().setError({ error, customCode: status })
   else project.value = data
 }
 
--- src/stores/error.ts
+++ src/stores/error.ts
@@ -1,11 +1,26 @@
-import type { CustomError } from '@/types/Error'
+import type { CustomError, ExtendedPostgrestError } from '@/types/Error'
+import type { PostgrestError } from '@supabase/supabase-js'
 
 export const useErrorStore = defineStore('error-store', () => {
-  const activeError = ref<null | CustomError>(null)
+  const activeError = ref<null | CustomError | ExtendedPostgrestError>(null)
 
-  const setError = ({ customCode, error }: { customCode: number; error: string }) => {
-    activeError.value = Error(error)
-    activeError.value.customCode = customCode
+  const setError = ({
+    customCode,
+    error
+  }: {
+    customCode: number
+    error: string | PostgrestError
+  }) => {
+    console.error('Error captured in store:', { customCode, error })
+
+    if (typeof error === 'string') {
+      activeError.value = Error(error)
+      activeError.value.customCode = customCode
+      return
+    }
+
+    activeError.value = error
+    ;(activeError.value as ExtendedPostgrestError).statusCode = customCode
   }
 
   return {
