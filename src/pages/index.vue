4abea30ea6bd2af750338e7ce994fcdf94854744
Mirko Bosetti <mirkobosetti1998@gmail.com>
2025-09-29 12:15:13 +0200

    supabase get

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,9 +2,7 @@
 
 <template>
   <div>
-    <h1>Home page</h1>
+    <h1>Home Page</h1>
     <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">Go to Project 1</RouterLink>
   </div>
 </template>
-
-<style scoped></style>
--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,9 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+import { ref } from 'vue'
+import type { Tables } from '../../../database/types'
+
+const projects = ref<Tables<'projects'>[] | null>(null)
+;(async () => {
+  const { data, error } = await supabase.from('projects').select()
+
+  if (error) console.log(error)
+  else projects.value = data
+})()
+</script>
 
 <template>
   <div>
     <h1>Projects page</h1>
     <RouterLink to="/">Go to Home</RouterLink>
+    <ul v-if="projects">
+      <li v-for="project in projects" :key="project.id">
+        <RouterLink :to="{ name: '/projects/[id]', params: { id: project.id } }">
+          {{ project.name }}
+        </RouterLink>
+      </li>
+    </ul>
   </div>
 </template>
 
