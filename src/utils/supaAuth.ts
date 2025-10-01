8c744f76d7758cd07b35f54a6f0450276642c8f9
Mirko Bosetti <bosetti@cving.com>
2025-10-01 18:17:30 +0200

    feat: enhance login error handling and display feedback to users

--- src/utils/supaAuth.ts
+++ src/utils/supaAuth.ts
@@ -28,9 +28,7 @@ export const login = async (formData: LoginForm) => {
     password: formData.password
   })
 
-  if (error) return console.error(error)
-
-  return true
+  return { error }
 }
 
 export const logout = async () => {
