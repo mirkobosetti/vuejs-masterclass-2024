8f1b60f030950e6fef89e5685a0d46f02af49cdf
Mirko Bosetti <bosetti@cving.com>
2025-10-03 18:14:31 +0200

    feat: add @formkit/vue dependency and implement task management UI
    - Added @formkit/vue version 1.6.9 to package.json and package-lock.json.
    - Updated Sidebar.vue to emit 'taskClicked' event and integrated DropdownMenu for task and project creation.
    - Modified AuthLayout.vue to handle task sheet visibility based on emitted events.
    - Created AppNewTask.vue component for task creation with a modal interface.
    - Implemented Sheet components (Sheet, SheetContent, SheetHeader, etc.) for reusable modal functionality using reka-ui.
    - Enhanced UI components with TypeScript support and improved styling.

---
+++ src/components/appNew/AppNewTask.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const isOpen = defineModel<boolean>()
+</script>
+
+<template>
+  <Sheet v-model:open="isOpen">
+    <SheetContent>
+      <SheetHeader>
+        <SheetTitle>Are you absolutely sure?</SheetTitle>
+      </SheetHeader>
+      <p>content</p>
+    </SheetContent>
+  </Sheet>
+</template>
