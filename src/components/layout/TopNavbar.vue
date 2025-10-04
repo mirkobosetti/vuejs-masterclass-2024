4795a272bd06dcc66de35d5ba5df04efc4ec8cc7
Mirko Bosetti <mirkobosetti1998@gmail.com>
2025-10-04 19:18:13 +0200

    feat: add dark mode toggle functionality and refactor TopNavbar layout

--- src/components/layout/TopNavbar.vue
+++ src/components/layout/TopNavbar.vue
@@ -1,4 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core'
+
+const { profile } = storeToRefs(useAuthStore())
+
+const isDarkMode = useDark()
+const toggleDarkMode = useToggle(isDarkMode)
+</script>
 
 <template>
   <nav class="h-16 border-b bg-muted/40 flex gap-2 justify-between px-6 items-center">
@@ -9,21 +16,33 @@
       ></iconify-icon>
       <Input class="w-full pl-8 bg-background" type="text" placeholder="Search ..." />
     </form>
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Avatar>
-          <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+
+    <div class="flex items-center gap-4">
+      <Button variant="ghost" size="icon" class="w-8 h-8" @click="toggleDarkMode()">
+        <Transition name="scale" mode="out-in">
+          <iconify-icon v-if="isDarkMode" icon="lucide:moon" class="text-lg"></iconify-icon>
+          <iconify-icon v-else icon="lucide:sun" class="text-lg"></iconify-icon>
+        </Transition>
+      </Button>
+
+      <div class="w-8">
+        <DropdownMenu v-if="profile">
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
   </nav>
 </template>
